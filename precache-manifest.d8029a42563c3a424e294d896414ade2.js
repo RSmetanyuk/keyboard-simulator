9e25fe35cad6e40e7f43b52a17e1e0475211c6c0
RSmetanyuk <rsmetanyuk@gmail.com>
2019-01-08 02:05:03 +0200

    Updates

---
+++ precache-manifest.d8029a42563c3a424e294d896414ade2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61ecbe43e092fe549995",
+    "url": "/RSmetanyuk/keyboard-simulator/static/js/runtime~main.61ecbe43.js"
+  },
+  {
+    "revision": "28304793e15318e46d5e",
+    "url": "/RSmetanyuk/keyboard-simulator/static/js/main.28304793.chunk.js"
+  },
+  {
+    "revision": "872b7d5f2ac19b6d0c37",
+    "url": "/RSmetanyuk/keyboard-simulator/static/js/1.872b7d5f.chunk.js"
+  },
+  {
+    "revision": "28304793e15318e46d5e",
+    "url": "/RSmetanyuk/keyboard-simulator/static/css/main.c57d63e6.chunk.css"
+  },
+  {
+    "revision": "57e3bb2c57c6a9357c3e27a0ca82d98e",
+    "url": "/RSmetanyuk/keyboard-simulator/index.html"
+  }
+];

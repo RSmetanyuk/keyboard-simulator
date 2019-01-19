aecd73b6b969924c2f1d25518ad3fe2a7e6b8dbc
RSmetanyuk <rsmetanyuk@gmail.com>
2019-01-19 22:57:24 +0200

    added localstorage

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ import ruducer from "./store/reducer";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { loadState, saveState } from "./store/localStorage";
+import { throttle } from "lodash";
 
-const store = createStore(ruducer, applyMiddleware(thunk));
+const persistedState = loadState();
+
+const store = createStore(ruducer, persistedState, applyMiddleware(thunk));
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
 
 ReactDOM.render(
   <Provider store={store}>

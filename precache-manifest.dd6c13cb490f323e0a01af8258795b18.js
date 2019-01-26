b5886a388772db68233355708589cca507a2d3c7
RSmetanyuk <rsmetanyuk@gmail.com>
2019-01-26 17:14:22 +0200

    Updates

---
+++ precache-manifest.dd6c13cb490f323e0a01af8258795b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "598564509c060d1da8ed",
+    "url": "/keyboard-simulator/static/js/runtime~main.59856450.js"
+  },
+  {
+    "revision": "e444b1c64287a10e26c2",
+    "url": "/keyboard-simulator/static/js/main.e444b1c6.chunk.js"
+  },
+  {
+    "revision": "5033b8a5d06e27375e88",
+    "url": "/keyboard-simulator/static/js/1.5033b8a5.chunk.js"
+  },
+  {
+    "revision": "e444b1c64287a10e26c2",
+    "url": "/keyboard-simulator/static/css/main.37a51918.chunk.css"
+  },
+  {
+    "revision": "5766c0d2e6d54becdf760bbe7cb7c778",
+    "url": "/keyboard-simulator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RSmetanyuk/keyboard-simulator/precache-manifest.ccf388d7a7a58a146ce6a147700d5ff0.js"
+  "/keyboard-simulator/precache-manifest.dd6c13cb490f323e0a01af8258795b18.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/RSmetanyuk/keyboard-simulator/index.html", {
+workbox.routing.registerNavigationRoute("/keyboard-simulator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
